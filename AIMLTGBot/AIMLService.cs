cc28a7657160fe1ec7ee5b3878e5100a91767e7c
kristinash <115154097+kristinash@users.noreply.github.com>
2025-12-29 17:21:29 +0300

    Add files via upload

---
+++ AIMLTGBot/AIMLService.cs
@@ -0,0 +1,40 @@
+﻿using AIMLbot;
+using System.Collections.Generic;
+
+namespace AIMLTGBot
+{
+    public class AIMLService
+    {
+        readonly Bot bot;
+        readonly Dictionary<long, User> users = new Dictionary<long, User>();
+
+        public AIMLService()
+        {
+            bot = new Bot();
+            bot.loadSettings();
+            bot.isAcceptingUserInput = false;
+            bot.loadAIMLFromFiles();
+            bot.isAcceptingUserInput = true;
+        }
+
+        public string Talk(long userId, string userName, string phrase)
+        {
+            var result = "";
+            User user;
+            if (!users.ContainsKey(userId))
+            {
+                user = new User(userId.ToString(), bot);
+                users.Add(userId, user);
+
+                // Вместо отправки сообщения в чат, просто тихо записываем имя в предикат
+                user.Predicates.addSetting("username", userName);
+            }
+            else
+            {
+                user = users[userId];
+            }
+            result += bot.Chat(new Request(phrase, user, bot)).Output;
+            return result;
+        }
+    }
+}
